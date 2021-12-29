69ca12a04a6a67a5a5622a54c4172c6dc69b22f0
SamSike <sameersikka7@gmail.com>
2021-12-30 04:51:34 +0530

    Created VaxMan Class

--- src/Main.cpp
+++ src/Main.cpp
@@ -21,17 +21,63 @@ using namespace std;
 static const int ORIGINAL_INFECTION = 4; //Original number of infections
 static const int MAX_INFECTION = 32 * ORIGINAL_INFECTION; //Max number of infections on the screen at one point of time
 static const int MULTIPLY_TIME = 30; //Time it takes for one infection to multiply
-static const float SQUARE_SIZE = 50.0; //Size of one square on the game
 static const int MAX_POINTS = 106; //Max points attainable
+static const float SQUARE_SIZE = 50.0; //Size of one square on the game
 
 static bool replay = false; //check if starts a new game
 static bool over = true; //check for the game to be over
 
-static float xIncrement = 0; // x movement on pacman	
-static float yIncrement = 0; // y movement on pacman
-static int rotation = 0; // orientation of pacman
+class VaxMan {
+
+	float positionX, positionY; // Co-ordinates of VaxMan
+	float xIncrement, yIncrement; // Movement of VaxMan
+	int rotation; // Orientation of VaxMan
+
+public:
+
+	//Resets vaxman to original configuration
+	void New() {
+		positionX = 1.5;
+		positionY = 1.5;
+		xIncrement = 0;
+		yIncrement = 0;
+		rotation = 0;
+	}
+
+	//Method to draw the vaxman character through consecutive circle algorithm
+	void DrawVaxMan() {
+
+		int x, y;
+		positionX += xIncrement;
+		positionY += yIncrement;
+		glBegin(GL_LINES);
+		glColor3f(1.0, 1.0, 0.0);
+
+		for (int k = 0; k < 32; k++) {
+			x = (float)k / 2.0 * cos((30 + 90 * rotation) * M_PI / 180.0) + (positionX * SQUARE_SIZE);
+			y = (float)k / 2.0 * sin((30 + 90 * rotation) * M_PI / 180.0) + (positionY * SQUARE_SIZE);
+
+			for (int i = 30; i < 330; i++) {
+				glVertex2f(x, y);
+				x = (float)k / 2.0 * cos((i + 90 * rotation) * M_PI / 180.0) + (positionX * SQUARE_SIZE);
+				y = (float)k / 2.0 * sin((i + 90 * rotation) * M_PI / 180.0) + (positionY * SQUARE_SIZE);
+				glVertex2f(x, y);
+			}
+		}
+		glEnd();
+	}
+
+	//Returns positionX
+	const float getX() { return positionX; }
+
+	//Returns positionY
+	const float getY() { return positionY; }
+
+	VaxMan() { New(); }
+}vaxman;
 
 class Infection {
+
 	static int numberOfInfections = 0;
 
 	int secondsAlive = 0;
@@ -49,9 +95,7 @@ public:
 	}
 
 	//Checks if infection is a Pandemic, i.e. if the game is over
-	static bool IsPandemic() const {
-		return (numberOfInfections == MAX_INFECTION);
-	}
+	static bool IsPandemic() const { return (numberOfInfections == MAX_INFECTION); }
 
 	//Checks if infection can multiply at current time
 	bool IsMultiply() {
@@ -173,6 +217,7 @@ static vector<int> obstaclesTop = { 2, 2, 3, 6, 3, 6, 4, 5, 4, 2, 5, 4, 5, 3, 6,
 static vector<int> obstaclesMiddle = { 2, 9, 3, 7, 3, 7, 4, 8, 4, 9, 5, 11, 5, 6, 6, 10, 6, 10, 7, 8, 7, 8, 8, 9, 6, 7, 7, 6, 8, 6, 9, 7, 10, 6, 9, 10, 9, 10, 8, 8, 11, 9, 10, 11, 11, 8, 12, 7, 12, 7, 13, 9 };
 static vector<int> obstaclesBottom = { 2, 10, 3, 13, 3, 13, 4, 12, 5, 12, 6, 13, 6, 13, 7, 11, 8, 11, 9, 13, 9, 13, 10, 12, 11, 12, 12, 13, 12, 13, 13, 10 };
 static deque<float> food = { 1.5, 1.5, 1.5, 2.5, 1.5, 3.5, 1.5, 4.5, 1.5, 5.5, 1.5, 6.5, 1.5, 7.5, 1.5, 8.5, 1.5, 9.5, 1.5, 10.5, 1.5, 11.5, 1.5, 12.5, 1.5, 13.5, 2.5, 1.5, 2.5, 6.5, 2.5, 9.5, 2.5, 13.5, 3.5, 1.5, 3.5, 2.5, 3.5, 3.5, 3.5, 4.5, 3.5, 6.5, 3.5, 8.5, 3.5, 9.5, 3.5, 10.5, 3.5, 11.5, 3.5, 13.5, 4.5, 1.5, 4.5, 4.5, 4.5, 5.5, 4.5, 6.5, 4.5, 7.5, 4.5, 8.5, 4.5, 11.5, 4.5, 12.5, 4.5, 13.5, 5.5, 1.5, 5.5, 2.5, 5.5, 5.5, 5.5, 10.5, 5.5, 11.5, 5.5, 13.5, 6.5, 2.5, 6.5, 3.5, 6.5, 4.5, 6.5, 5.5, 6.5, 7.5, 6.5, 10.5, 6.5, 13.5, 7.5, 5.5, 7.5, 6.5, 7.5, 7.5, 7.5, 9.5, 7.5, 10.5, 7.5, 11.5, 7.5, 12.5, 7.5, 13.5, 8.5, 2.5, 8.5, 3.5, 8.5, 4.5, 8.5, 5.5, 8.5, 7.5, 8.5, 10.5, 8.5, 13.5, 9.5, 1.5, 9.5, 2.5, 9.5, 5.5, 9.5, 10.5, 9.5, 11.5, 9.5, 13.5, 10.5, 1.5, 10.5, 4.5, 10.5, 5.5, 10.5, 6.5, 10.5, 7.5, 10.5, 8.5, 10.5, 11.5, 10.5, 12.5, 10.5, 13.5, 11.5, 1.5, 11.5, 2.5, 11.5, 3.5, 11.5, 4.5, 11.5, 5.5, 11.5, 6.5, 11.5, 8.5, 11.5, 9.5, 11.5, 10.5, 11.5, 11.5, 11.5, 13.5, 12.5, 1.5, 12.5, 6.5, 12.5, 9.5, 12.5, 13.5, 13.5, 1.5, 13.5, 2.5, 13.5, 3.5, 13.5, 4.5, 13.5, 5.5, 13.5, 6.5, 13.5, 7.5, 13.5, 8.5, 13.5, 9.5, 13.5, 10.5, 13.5, 11.5, 13.5, 12.5, 13.5, 13.5 };
+
 static vector<vector<bool>> bitmap; // 2d image of which squares are blocked and which are clear for vaxman to move in 
 bool* keyStates = new bool[256]; // record of all keys pressed 
 int points = 0; // total points collected
@@ -217,7 +262,8 @@ void DrawLabyrinth() {
 
 	//Border
 	for (int i = 0; i < border.size(); i = i + 4) {
-		glRectf(border.at(i) * SQUARE_SIZE, border.at(i + 1) * SQUARE_SIZE, border.at(i + 2) * SQUARE_SIZE, border.at(i + 3) * SQUARE_SIZE);
+		glRectf(border.at(i) * SQUARE_SIZE, border.at(i + 1) * SQUARE_SIZE, 
+			border.at(i + 2) * SQUARE_SIZE, border.at(i + 3) * SQUARE_SIZE);
 	}
 
 	//Obstacles
@@ -233,10 +279,10 @@ void DrawLabyrinth() {
 }
 
 //Method to check if the food has been eaten
-bool IsFoodEaten(const int& x, const int& y, float pacmanX, float pacmanY) {
+bool IsFoodEaten(const int& x, const int& y) {
 
-	if (x >= pacmanX - 16.0 * cos(359 * M_PI / 180.0) && x <= pacmanX + 16.0 * cos(359 * M_PI / 180.0)) {
-		if (y >= pacmanY - 16.0 * cos(359 * M_PI / 180.0) && y <= pacmanY + 16.0 * cos(359 * M_PI / 180.0)) {
+	if (x >= vaxman.getX() - 16.0 * cos(359 * M_PI / 180.0) && x <= vaxman.getX() + 16.0 * cos(359 * M_PI / 180.0)) {
+		if (y >= vaxman.getY() - 16.0 * cos(359 * M_PI / 180.0) && y <= vaxman.getY() + 16.0 * cos(359 * M_PI / 180.0)) {
 			return true;
 		}
 	}
@@ -244,13 +290,13 @@ bool IsFoodEaten(const int& x, const int& y, float pacmanX, float pacmanY) {
 }
 
 //Method to draw all the food left and delete the ate one
-void DrawFood(float pacmanX, float pacmanY) {
+void DrawFood() {
 
 	deque<float> temp;
 
 	//check if the food has not been eaten
 	for (int i = 0; i < food.size(); i = i + 2) {
-		if (!IsFoodEaten(food.at(i) * SQUARE_SIZE, food.at(i + 1) * SQUARE_SIZE, pacmanX, pacmanY)) {
+		if (!IsFoodEaten(food.at(i) * SQUARE_SIZE, food.at(i + 1) * SQUARE_SIZE, vaxman.getX(), vaxman.getY())) {
 			temp.push_back(food.at(i));
 			temp.push_back(food.at(i + 1));
 		}
@@ -272,41 +318,18 @@ void DrawFood(float pacmanX, float pacmanY) {
 	glEnd();
 }
 
-//Method to draw the vaxman character through consicutive circle algorithm
-void DrawVaxman(float positionX, float positionY, float rotation) {
-
-	int x, y;
-	glBegin(GL_LINES);
-	glColor3f(1.0, 1.0, 0.0);
-
-	for (int k = 0; k < 32; k++) {
-		x = (float)k / 2.0 * cos((30 + 90 * rotation) * M_PI / 180.0) + (positionX * SQUARE_SIZE);
-		y = (float)k / 2.0 * sin((30 + 90 * rotation) * M_PI / 180.0) + (positionY * SQUARE_SIZE);
-		for (int i = 30; i < 330; i++) {
-			glVertex2f(x, y);
-			x = (float)k / 2.0 * cos((i + 90 * rotation) * M_PI / 180.0) + (positionX * SQUARE_SIZE);
-			y = (float)k / 2.0 * sin((i + 90 * rotation) * M_PI / 180.0) + (positionY * SQUARE_SIZE);
-			glVertex2f(x, y);
-		}
-	}
-	glEnd();
-}
-
 //Destroys infection in contact with VaxMan
 void DestroyInfection(const int& deleteIndex) {
+
 	infectionArray->at(deleteIndex) = infectionArray->back();
 	infectionArray->pop_back();
 }
 
 //Method to set the pressed key
-void KeyPressed(const unsigned char& key, int x, int y) {
-	keyStates[key] = true;
-}
+void KeyPressed(const unsigned char& key, int x, int y) { keyStates[key] = true; }
 
 //Method to unset the released key
-void KeyUp(const unsigned char& key, int x, int y) {
-	keyStates[key] = false;
-}
+void KeyUp(const unsigned char& key, int x, int y) { keyStates[key] = false; }
 
 //Method to start the infection
 void StartInfection() {
@@ -392,8 +415,8 @@ void KeyOperations() {
 //Method to check if the game is over
 void IsGameOver() {
 
-	int pacmanX = (int)(1.5 + xIncrement);
-	int pacmanY = (int)(1.5 + yIncrement);
+	int vaxman.getX() = (int)(1.5 + xIncrement);
+	int vaxman.getY() = (int)(1.5 + yIncrement);
 	int monster1X = (int)(monster1[0]);
 	int monster1Y = (int)(monster1[1]);
 	int monster2X = (int)(monster2[0]);
@@ -403,16 +426,16 @@ void IsGameOver() {
 	int monster4X = (int)(monster4[0]);
 	int monster4Y = (int)(monster4[1]);
 
-	if (pacmanX == monster1X && pacmanY == monster1Y) {
+	if (vaxman.getX() == monster1X && vaxman.getY() == monster1Y) {
 		over = true;
 	}
-	if (pacmanX == monster2X && pacmanY == monster2Y) {
+	if (vaxman.getX() == monster2X && vaxman.getY() == monster2Y) {
 		over = true;
 	}
-	if (pacmanX == monster3X && pacmanY == monster3Y) {
+	if (vaxman.getX() == monster3X && vaxman.getY() == monster3Y) {
 		over = true;
 	}
-	if (pacmanX == monster4X && pacmanY == monster4Y) {
+	if (vaxman.getX() == monster4X && vaxman.getY() == monster4Y) {
 		over = true;
 	}
 	if (points == MAX_POINTS) {
@@ -489,7 +512,7 @@ void LossScreen() {
 //Method to display the results of the game at the ends
 void ResultsDisplay() {
 
-	if (points == 106)
+	if (points == MAX_POINTS)
 		win();
 	else
 		loss();
@@ -540,7 +563,7 @@ void Display() {
 		if (!over) {
 			DrawLabyrinth();
 			DrawFood((1.5 + xIncrement) * SQUARE_SIZE, (1.5 + yIncrement) * SQUARE_SIZE);
-			DrawVaxMan(1.5 + xIncrement, 1.5 + yIncrement, rotation);
+			vaxman.DrawVaxMan();
 
 			for (int i = 0; i < infectionArray.size(); i++) {
 				infectionArray->at(i).UpdateInfection();
@@ -567,7 +590,6 @@ void Reshape(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
-
 
 //Main functions that controls the running of the game
 int main(int argc, char** argv) {
